b4ff74cc72257febc4f522b2c25d68fadd66aa20
knicKnack-Mark <markneilbertiz21@gmail.com>
2026-09-16 21:25:52 +0800

    feat: add MyComponent for displaying a greeting message

---
+++ src/pages/Pets.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Default export function matching the file name
+export default function MyComponent() {
+  return (
+    <div className="component-container">
+      <h1>Hello, World!</h1>
+    </div>
+  );
+}
